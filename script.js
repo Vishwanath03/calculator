20a9171c1e587bb59ef6b9c36e1aa23011f0a87a
Vishwanath03 <meruguvishwa03@outlook.com>
2023-11-05 20:48:31 +0530

    now every thing is working fine for calculation except paranthesis for doing multiplication

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
-const audio = new Audio();
-audio.src = "btnsound.mp3";
-
-const song = new Audio();
-song.src = "magic-fairy.mp3";
-
-let answer = document.getElementById("answer");
-let expression = "";
-
-function clearDisplay() {
-  expression = "";
-  answer.innerHTML = "0";
-}
-
-function appendDigit(digit) {
-  expression += digit;
-  answer.innerHTML = expression;
-}
-function appendOperator(operator) {
-  expression += appendOperator;
-  answer.innerHTML = expression;
-}
-function appendDecimal() {
-  expression += ".";
-  answer.innerHTML = expression;
-}
-function evalexpr() {
-  let result = eval(expression);
-  answer.innerHTML = result;
-  // let que = document.getElementById("question");
-  // question.innerHTML = expression;
-}
+let question = document.getElementById("question");
+let result = document.getElementById("answer");
+let buttons = document.querySelectorAll("button");
+let s = "";
+let q = "";
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (b) => {
+    if (b.target.innerHTML == "=") {
+      var audio2 = new Audio("magic-fairy.mp3");
+      audio2.play();
+      q = s;
+      question.value = q;
+      s = eval(s);
+      result.value = s;
+    } else if (b.target.innerHTML == "AC") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = "";
+      question.value = "Enter question...";
+      result.value = s;
+    } else if (b.target.innerHTML == "Del") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s.substring(0, s.length - 1);
+      result.value = s;
+    } else if (b.target.innerHTML == "(") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + b.target.innerHTML;
+      result.value = s;
+    } else if (b.target.innerHTML == ")") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + b.target.innerHTML;
+      result.value = s;
+    } else if (b.target.innerHTML == "÷") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + "/";
+      result.value = s;
+    } else if (b.target.innerHTML == "–") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + "-";
+      result.value = s;
+    } else if (b.target.innerHTML == "×") {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + "*";
+      result.value = s;
+    } else {
+      var audio = new Audio("btnsound.mp3");
+      audio.play();
+      s = s + b.target.innerHTML;
+      result.value = s;
+    }
+  });
+});
